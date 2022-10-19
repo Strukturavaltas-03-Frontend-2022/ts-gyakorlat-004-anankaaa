f667a53bcde30c0d15e8737e74aa8e241e40f684
Anankaaa <hegyaljai83@hotmail.com>
2022-10-19 21:05:37 +0200

    ready

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero";
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,31 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {
+        id: 1,
+        name: "Name1",
+        sex: "male",
+        age: 10,
+        health: 5,
+    },
+
+    {
+        id: 2,
+        name: "Name2",
+        sex: "male",
+        age: 11,
+        health: 8,
+    },
+
+    {
+        id: 3,
+        name: "Name3",
+        sex: "female",
+        age: 20,
+        health: 2,
+    }
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +42,27 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+
+    {
+        id: 1,
+        name: "namee1",
+        wings: 2,
+        wheels: 4,
+        clan: "klan1"
+    },
+    {
+        id: 2,
+        name: "namee2",
+        wings: 0,
+        wheels: 4,
+        clan: "klan2"
+    },
+    {
+        id: 3,
+        name: "namee3",
+        wings: 4,
+        wheels: 6,
+        clan: "klan3"
+    },
+];
